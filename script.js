f8dc191b7af224eaed756a6016c6f81d55199327
bibinr <bibinr@users.noreply.github.com>
2024-03-19 18:45:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -16,12 +16,33 @@ setTimeout(() => {
     document.getElementsByTagName('body')[0].style.color = 'pink';
     document.getElementsByTagName('body')[0].style.background = 'pink'; 
   }
- confetti({
-            particleCount: 100,
-            spread: 70,
-            origin: { y: 0.6 },
-            colors: ['#add8e6', '#87CEEB', '#1E90FF'], // Shades of blue
-        });
+ // confetti({
+ //            particleCount: 100,
+ //            spread: 70,
+ //            origin: { y: 0.6 },
+ //            colors: ['#add8e6', '#87CEEB', '#1E90FF'], // Shades of blue
+ //        });
+
+var duration = 15 * 1000;
+var animationEnd = Date.now() + duration;
+var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+var interval = setInterval(function() {
+  var timeLeft = animationEnd - Date.now();
+
+  if (timeLeft <= 0) {
+    return clearInterval(interval);
+  }
+
+  var particleCount = 50 * (timeLeft / duration);
+  // since particles fall down, start a bit higher than random
+  confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } });
+  confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } });
+}, 250);
   
 },5000)
 
